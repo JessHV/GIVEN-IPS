c6455fe4e4874454b94b4bc79a48acec6442b8e2
CesarVasquez2001 <cvasqueza@unsa.edu.pe>
2021-07-29 16:37:19 -0500

    rutas para foro

--- PF/src/app/foro/foro-routing.module.ts
+++ PF/src/app/foro/foro-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { ComentarioComponent } from './comentario/comentario.component';
 import { ForoComponent } from './foro/foro.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ForoComponent,
+  },
+  {
+    path: 'myposts',
+    component: MyPostsComponent,
+  },
+  {
+    path: 'add',
+    component: AddComponent,
+  },
+  {
+    path: 'myposts/edit/:id',
+    component: AddComponent,
+  }, 
+  {
+    path: 'comentario/:id',
+    component: ComentarioComponent,
   },
   {
     path: '**',
@@ -15,6 +34,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class ForoRoutingModule { }
