57a58a720b20e6d236037ad7384e386d17d286c5
AlexRTC <aturpoco@unsa.edu.pe>
2021-07-30 14:33:46 -0500

    Filtro de genero para tipos de metodos

--- PF/src/app/busqueda/filtros/filtros.component.ts
+++ PF/src/app/busqueda/filtros/filtros.component.ts
@@ -14,6 +14,7 @@ export class FiltrosComponent implements OnInit {
   MethodList:any=[];
   MethodNameFilter:string="";
   TypeMethodFilter:string="";
+  GenderMethodFilter:string="";
   MethodListWithoutFilter:any=[];
   public localMethods:any = [
     {
@@ -23,7 +24,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Basado en la abstinencia sexual durante la fase fértil del ciclo menstrual para evitar un embarazo",
       "MetodoImagen": "metodo-1.png",
       "MetodoTipo": "MAP",
-      "MetodoTasa": "Falla teórica o de uso perfecto 9 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 25 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 9 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 25 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 2,
@@ -32,7 +34,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Basado en la abstinencia sexual durante la fase fértil del ciclo menstrual para evitar un embarazo",
       "MetodoImagen": "metodo-2.jpg",
       "MetodoTipo": "MAP",
-      "MetodoTasa": "Falla teórica o de uso perfecto 3 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 25 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 3 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 25 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 3,
@@ -41,7 +44,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Basado en la abstinencia sexual durante la fase fértil del ciclo menstrual para evitar un embarazo",
       "MetodoImagen": "metodo-3.jpg",
       "MetodoTipo": "MAP",
-      "MetodoTasa": "Falla teórica o de uso perfecto 3 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 25 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 3 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 25 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 4,
@@ -50,7 +54,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Supresión de la ovulación ocasionada por el incremento de la hormona prolactina como consecuencia de la lactancia materna exclusiva",
       "MetodoImagen": "metodo-4.jpg",
       "MetodoTipo": "MELA",
-      "MetodoTasa": "Falla teórica o de uso perfecto 1 embarazo por 100 mujeres, en los primeros seis meses post parto.|Falla de uso o uso típico 2 embarazos por 100 mujeres, en los primeros seis meses postparto."
+      "MetodoTasa": "Falla teórica o de uso perfecto 1 embarazo por 100 mujeres, en los primeros seis meses post parto.|Falla de uso o uso típico 2 embarazos por 100 mujeres, en los primeros seis meses postparto.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 5,
@@ -59,7 +64,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Impide que los espermatozoides tengan acceso al tracto reproductor femenino",
       "MetodoImagen": "metodo-5.jpg",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Falla teórica o de uso perfecto 2 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 15 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 2 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 15 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Masculino"
     },
     {
       "MetodoId": 6,
@@ -68,7 +74,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Impide el pasaje de los espermatozoides al tracto reproductor femenino",
       "MetodoImagen": "metodo-6.jpg",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Falla teórica o de uso perfecto 5 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 21 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 5 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 21 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 7,
@@ -77,7 +84,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Inactivan o destruyen los espermatozoides en la vagina",
       "MetodoImagen": "metodo-7.jpg",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Falla teórica o de uso perfecto 18 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 29 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 18 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 29 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 8,
@@ -86,7 +94,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Supresión de la ovulación y espesamiento de moco cervical",
       "MetodoImagen": "metodo-8.jpg",
       "MetodoTipo": "AH",
-      "MetodoTasa": "Falla teórica o de uso perfecto 0.3 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 8 embarazos por cada 100 mujeres en el primer año de uso."
+      "MetodoTasa": "Falla teórica o de uso perfecto 0.3 embarazos por cada 100 mujeres en el primer año de uso.|Falla de uso o uso típico 8 embarazos por cada 100 mujeres en el primer año de uso.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 9,
@@ -95,16 +104,18 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Supresión de la ovulación y espesamiento del moco cervical, impidiendo la penetración de los espermatozoides",
       "MetodoImagen": "metodo-9.jpg",
       "MetodoTipo": "AH",
-      "MetodoTasa": "Falla teórica 0.05 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 3 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica 0.05 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 3 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 10,
       "MetodoNombre": "Parche Hormonal Combinado",
-      "MetodoDescripcion": "parche adhesivo pequeño (20 centímetros cuadrados no es 4 x 4) que se coloca en la piel. El parche libera estrógeno y progestina a través de la piel y en la sangre",
+      "MetodoDescripcion": "Parche adhesivo pequeño (20 centímetros cuadrados no es 4 x 4) que se coloca en la piel. El parche libera estrógeno y progestina a través de la piel y en la sangre",
       "MetodoMecanismo": "Supresión de la ovulación y espesamiento del moco cervical, impidiendo la penetración de los espermatozoides",
       "MetodoImagen": "metodo-10.jpg",
       "MetodoTipo": "AH",
-      "MetodoTasa": "Falla teórica 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 8 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 8 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 11,
@@ -113,7 +124,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Supresión de la ovulación y espesamiento del moco cervical, impidiendo la penetración de los espermatozoides",
       "MetodoImagen": "metodo-11.png",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Falla teórica 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 8 embarazos por cada 100 mujeres en el primer año"
+      "MetodoTasa": "Falla teórica 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 8 embarazos por cada 100 mujeres en el primer año",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 12,
@@ -122,7 +134,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Inhiben parcialmente la ovulación y espesa el moco cervical, dificultando la penetración de los espermatozoides",
       "MetodoImagen": "metodo-12.jpg",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Falla teórica es 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico es 8 embarazos por cada 100 mujeres en el primer año"
+      "MetodoTasa": "Falla teórica es 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico es 8 embarazos por cada 100 mujeres en el primer año",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 13,
@@ -131,7 +144,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Espesamiento del moco cervical, impidiendo la penetración de los espermatozoides y supresión de la ovulación en un 50%",
       "MetodoImagen": "metodo-13.jpg",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Falla teórica 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 3 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica 0.3 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 3 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 14,
@@ -140,7 +154,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Espesamiento del moco cervical, impidiendo la penetración de los espermatozoides, supresión de la ovulación y reducción del transporte de los óvulos en las Trompas de Falopio",
       "MetodoImagen": "metodo-14.png",
       "MetodoTipo": "MB",
-      "MetodoTasa": "Es uno de los métodos más eficaces y de mayor duración.|0.05% de falla teórica y en el uso típico (1 en 100 mujeres)."
+      "MetodoTasa": "Es uno de los métodos más eficaces y de mayor duración.|0.05% de falla teórica y en el uso típico (1 en 100 mujeres).",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 15,
@@ -149,7 +164,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Afectan la capacidad de los espermatozoides de pasar a través de la cavidad uterina ya sea inmovilizándolos o destruyéndolos e interfieren en el proceso reproductor antes que ocurra la fertilización",
       "MetodoImagen": "metodo-15.jpg",
       "MetodoTipo": "DIU",
-      "MetodoTasa": "Falla teórica  de 0.6 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 0.8 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica  de 0.6 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 0.8 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 16,
@@ -158,7 +174,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Espesamiento del moco cervical, impidiendo la penetración de los espermatozoides y supresión de la ovulación",
       "MetodoImagen": "metodo-16.jpg",
       "MetodoTipo": "DIU",
-      "MetodoTasa": "Falla teórica 0.1 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 0.1 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica 0.1 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 0.1 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 17,
@@ -167,7 +184,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Es la oclusión y sección de las trompas de Falopio para impedir la unión del espermatozoide con el óvulo",
       "MetodoImagen": "metodo-17.jpg",
       "MetodoTipo": "AQV",
-      "MetodoTasa": "Falla teórica 0.5 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 0.5 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica 0.5 embarazos por cada 100 mujeres en el primer año.|Falla de uso típico 0.5 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 18,
@@ -176,7 +194,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "La oclusión de los conductos deferentes que impide la salida de los espermatozoides, evitando la unión con el óvulo",
       "MetodoImagen": "metodo-18.png",
       "MetodoTipo": "AQV",
-      "MetodoTasa": "Falla teórica 0.15 embarazos por cada 100 mujeres en el primer año.|Falla de uso 0.15 embarazos por cada 100 mujeres en el primer año."
+      "MetodoTasa": "Falla teórica 0.15 embarazos por cada 100 mujeres en el primer año.|Falla de uso 0.15 embarazos por cada 100 mujeres en el primer año.",
+      "MetodoGenero": "Masculino"
     },
     {
       "MetodoId": 19,
@@ -185,7 +204,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Inhibe la ovulación y espesa el moco cervical",
       "MetodoImagen": "metodo-19.jpeg",
       "MetodoTipo": "AE",
-      "MetodoTasa": "Cuando más pronto sea su uso, mayor será su eficacia.|Los anticonceptivos combinados (Método de Yuzpe) hasta 75%"
+      "MetodoTasa": "Cuando más pronto sea su uso, mayor será su eficacia.|Los anticonceptivos combinados (Método de Yuzpe) hasta 75%",
+      "MetodoGenero": "Femenino"
     },
     {
       "MetodoId": 20,
@@ -194,7 +214,8 @@ export class FiltrosComponent implements OnInit {
       "MetodoMecanismo": "Inhibe la ovulación y espesa el moco cervical",
       "MetodoImagen": "metodo-20.jpg",
       "MetodoTipo": "AE",
-      "MetodoTasa": "Cuando más pronto sea su uso, mayor será su eficacia.|Los de solo progestágeno (levonorgestrel) hasta 95%."
+      "MetodoTasa": "Cuando más pronto sea su uso, mayor será su eficacia.|Los de solo progestágeno (levonorgestrel) hasta 95%.",
+      "MetodoGenero": "Femenino"
     }
   ];
 
@@ -217,6 +238,8 @@ export class FiltrosComponent implements OnInit {
   // Función para filtrar los métodos anticonceptivos
   filterMethods() {
     var MethodNameFilter = this.MethodNameFilter;
+    this.TypeMethodFilter="";
+    this.GenderMethodFilter="";
 
     this.MethodList = this.MethodListWithoutFilter.filter(function(el:any) {
       return el.MetodoNombre.toString().toLowerCase().includes(MethodNameFilter.toString().trim().toLowerCase());
@@ -226,9 +249,22 @@ export class FiltrosComponent implements OnInit {
   // Función para filtrar métodos anticonceptivos según su tipo
   filterType() {
     var TypeMethodFilter = this.TypeMethodFilter;
+    this.MethodNameFilter="";
+    this.GenderMethodFilter="";
 
     this.MethodList = this.MethodListWithoutFilter.filter(function(el:any) {
       return el.MetodoTipo.toString().toLowerCase().includes(TypeMethodFilter.toString().trim().toLowerCase());
+    });
+  }
+
+  // Función para filtrar métodos anticonceptivos según el género de la persona
+  filterGender() {
+    var GenderMethodFilter = this.GenderMethodFilter;
+    this.TypeMethodFilter="";
+    this.MethodNameFilter="";
+
+    this.MethodList = this.MethodListWithoutFilter.filter(function(el:any) {
+      return el.MetodoGenero.toString().toLowerCase().includes(GenderMethodFilter.toString().trim().toLowerCase());
     });
   }
 
